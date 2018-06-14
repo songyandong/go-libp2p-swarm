ecef7be1653b05c9762d538d91b356981c7aabf9
Steven Allen <steven@stebalien.com>
2018-06-14 15:48:25 -0700

    switch to a per DialPeer/NewStream timeout defined in go-libp2p-net
    The global per-peer dial timeout had a significant drawback: When dialing many
    peers, this timeout could cause libp2p to cancel dials while they were still
    stuck in the limiter.
    
    A better but more complicated approach is a time budget system but we can
    implement that later.
    
    This change simply applies the limit to each `DialPeer`/`NewStream` call
    independently and makes it easy to override. While old timeout tried to
    account for how much we're willing to spend dialing a single peer, this new
    timeout tries to account for the amount of time a single "client" is willing to
    wait for a dial to complete before they no longer care.

--- swarm.go
+++ swarm.go
@@ -7,7 +7,6 @@ import (
 	"strings"
 	"sync"
 	"sync/atomic"
-	"time"
 
 	logging "github.com/ipfs/go-log"
 	"github.com/jbenet/goprocess"
@@ -30,11 +29,6 @@ var ErrSwarmClosed = errors.New("swarm closed")
 // filtered address. You shouldn't see this error unless some underlying
 // transport is misbehaving.
 var ErrAddrFiltered = errors.New("address filtered")
-
-// DialTimeout is the maximum duration a Dial is allowed to take.
-// This includes the time between dialing the raw network connection,
-// protocol selection as well the handshake, if applicable.
-var DialTimeout = 60 * time.Second
 
 // Swarm is a connection muxer, allowing connections to other peers to
 // be opened and closed, while still using the same Chan for all
